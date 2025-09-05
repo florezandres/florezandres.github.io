79ca7112779241baa3f776f9ba7d4839938857bb
florezandres <andres.f1850@gmail.com>
2025-09-05 01:44:43 -0500

    Initial visual structure idea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskbarComponent } from './layout/taskbar/taskbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/layout/taskbar/taskbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taskbar',
+  imports: [],
+  templateUrl: './taskbar.component.html',
+  styleUrl: './taskbar.component.css'
+})
+export class TaskbarComponent {
+
+}
