f5c1f62e002148eeaa5dc5a9a5c3184f8f74bb66
Andrés Florez <andres.f1850@gmail.com>
2026-05-31 00:11:39 -0500

    Initial layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        loadComponent: () => import('./routes/home/home').then(m => m.Home)
+    }
+
+];
